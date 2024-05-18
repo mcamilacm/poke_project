ba6b9a5a3b9f288e80bcfd1e888bba187565c880
Camila <mariacamilacalle1@gmail.com>
2024-05-18 16:20:40 -0500

    card css

--- src/components/CardSelected.jsx
+++ src/components/CardSelected.jsx
@@ -5,17 +5,25 @@ const CardSelected = () => {
     const { selectedPokemon } = useContext(PostContext);
 
     if (!selectedPokemon) {
-        return <div>Select a Pokemon to see details</div>;
+        return <div className="select_pokemon">Select a Pokemon to see details</div>;
     }
 
     return (
         <div className="card">
+
+            <div className="pokemon_img">
             <h2>{selectedPokemon.name}</h2>
             <img src={selectedPokemon.sprites.front_default} alt={selectedPokemon.name} />
+            </div>
+            <div className="pokemon_info">
             <p>Height: {selectedPokemon.height}</p>
+
+
+
             <p>Weight: {selectedPokemon.weight}</p>
             <p>Base Experience: {selectedPokemon.base_experience}</p>
             <p>Abilities: {selectedPokemon.abilities.map(ability => ability.ability.name).join(', ')}</p>
+            </div>
         </div>
     );
 };
